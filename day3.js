cc43d24fe002cdfd008a872469592746ca863441
himanshu <himanshu01444@gmail.com>
2022-03-10 00:05:00 +0530

    initial commit

---
+++ day3.js
@@ -0,0 +1,59 @@
+//using literals
+
+// let arr = [];  //empty array
+
+// console.log("arr", arr);
+
+// //using constructor function
+
+// let anotherArr = new Array();
+
+// console.log("anotherArr",anotherArr);
+
+
+// let arr =[1,2,3,4,5,6];
+// console.log("arr",arr);
+
+// let anotherArr = new Array(1,2,5,7,8,4,8);
+// console.log("anotherArr", anotherArr);
+
+// //push,pop,shift,unshift
+
+// let values =["a","e","i","o","u"];
+
+// console.log(values);
+// values.push(1);//add value
+// console.log(values);
+// values.pop();//remove value
+// console.log(values);
+// values.shift();//remove first value
+// console.log(values);
+// values.unshift("a");//add fisrt value
+// console.log(values);
+
+
+// function countString (arr){
+//     let count = 0;
+//     let tempArr = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
+//         if (typeof(element)==="string") count++;
+//          if (typeof(element)==="string") tempArr.push(element)
+//     }
+//     return tempArr;
+// }
+
+// const result = countString(["a", "b", "c", 1, 3, 5]);
+// console.log(result);
+
+// let arr =[1,2,3,4,4,5,5];
+
+// const value1 =arr.some(element =>element>2);
+// const value2 =arr.every(element =>element>2);
+
+// console.log(value1, value2); //true false
+
+
+
+
+
